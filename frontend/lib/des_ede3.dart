104ef2cdc6444a217e27b34f6ea97873b1a4183d
VatanakChamroeun <ch.vatanak01@gmail.com>
2022-07-05 16:03:41 +0700

    refactor: update DES to DES3

--- frontend/lib/des_ede3.dart
+++ frontend/lib/des_ede3.dart
@@ -4,8 +4,8 @@ import 'package:dart_des/dart_des.dart';
 
 class DesEde3 {
   static String encrypt(String plainText, String key) {
-    List<int> encodeKey = utf8.encode(key).getRange(0, 8).toList();
-    DES cipher = DES(key: encodeKey, mode: DESMode.ECB);
+    List<int> encodeKey = utf8.encode(key).getRange(0, 16).toList();
+    var cipher = DES3(key: encodeKey, mode: DESMode.ECB);
 
     plainText += '\x00' * (8 - plainText.length % 8);
     List<int> encrypted = cipher.encrypt(ascii.encode(plainText));
@@ -15,8 +15,8 @@ class DesEde3 {
   }
 
   static String decrypt(String cipherText, String key) {
-    List<int> decodeKey = key.substring(0, 8).codeUnits;
-    DES cipher = DES(key: decodeKey, mode: DESMode.ECB);
+    List<int> decodeKey = key.substring(0, 16).codeUnits;
+    var cipher = DES3(key: decodeKey, mode: DESMode.ECB);
     Uint8List plainTextBase64 = base64Decode(cipherText);
     List<int> s = cipher.decrypt(plainTextBase64);
 
